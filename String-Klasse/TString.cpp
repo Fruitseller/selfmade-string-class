d7d29faa421054215bb6acfc1e42cc72712c3a23
Peter Bronkalla <peterbronkalla@gmail.com>
2013-11-08 16:24:56 +0100

    Now really deleted directory

--- String-Klasse/TString.cpp
+++
@@ -1,19 +0,0 @@
-#include "TString.h"
-
-	int x;
-
-	TString::TString()
-	{
-		this->x = 0;
-	}
-
-	TString::TString (int number)	{		this->x = number;	}	TString::~TString()
-	{
-		
-	}
-
-	int TString::test()
-	{
-		return this->x;	
-	}
-
--- String-Klasse/TString.h
+++
@@ -1,14 +0,0 @@
-#ifndef __TSTRING_H__
-#define __TSTRING_H__
-
-class TString {  
-	private:   
-		int x;
-
-	public: 
-		TString();
-		TString (int); 		~TString() ;
-		int test(); 
-};  
-
-#endif 
